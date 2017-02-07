4549e8d96d8461d9c3cbd346e5d44fc55d5a8e26
Liching Li <P696328@aur.national.com.au>
2017-02-07 14:58:47 +1100

    Move the JSON definition comment to the ZingChart class
    The definition in comment form was located in the Exporter class. This
    is not the best place for it. Moved to the ZingChart class instead.

--- SVNAnalyser.Core/Exporter.cs
+++ SVNAnalyser.Core/Exporter.cs
@@ -7,29 +7,7 @@ namespace SVNAnalyser.Core
 {
     public class Exporter
     {
-        /* export a json definition that is recognised by the ZingChart library
-         
-         * ZingChart JSON definition:
-         
-            "graphset": [{
-                   "type":"pie",
-			       "plot": {
-			   			    "tooltip": {"text":"%t"}, 
-						    "value-box": { "font-size" : 0, 
-						 			       "placement":"in", 
-						 			       "text":"%t", 
-						 			       "offsetR": "30%", 
-									       "rules": [{"rule": "%v === 0","text": ""}]
-									     }
-					       },
-				    "title":{"font-size" : 10,"text":"dummy"},
-				    "series":[{"values":[0.1],"text":"p694486"},{"values":[0.1],"text":"P695079"},{"values":[0.1],"text":"p695089"},{"values":[0.1],"text":"p696253"},{"values":[0.1],"text":"P696328"},{"values":[0.1],"text":"p696693"},{"values":[0.1],"text":"p697038"},{"values":[0.1],"text":"P697043"},{"values":[0.1],"text":"P698972"},{"values":[0.1],"text":"p699912"},{"values":[0.1],"text":"p700246"}]
-                 },  
-		         { "type":"pie",
-                   "plot": {"tooltip":{"text":"%t"}, "value-box" : {"font-size" : 0, "placement":"in", "text":"%t", "offsetR": "30%", "rules": [{"rule": "%v === 0","text": ""}]}},"title":{"font-size" : 10,"text":"/NavCoreServices/AusmaqImportTRTBatchProcessHandler.CLA"},"series":[{"values":[0],"text":"p694486"},{"values":[0],"text":"P695079"},{"values":[0],"text":"p695089"},{"values":[0],"text":"p696253"},{"values":[0.1],"text":"P696328"},{"values":[0],"text":"p696693"},{"values":[0.1],"text":"p697038"},{"values":[0],"text":"P697043"},{"values":[0],"text":"P698972"},{"values":[0.8],"text":"p699912"},{"values":[0],"text":"p700246"}]
-                 },
-                 etc...
-        */
+
         public ZingChart toZingChart(string outputPath = null)
         {
             ZingChart zingchart = new ZingChart();
