556a6eb32df7980b26043f0431c1684846e8d00d
Liching Li <P696328@aur.national.com.au>
2017-02-07 12:11:45 +1100

    Add simple start point into program

--- SVNAnalyser/Program.cs
+++ SVNAnalyser/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SVNAnalyser.Core;
 
 namespace SVNAnalyser
 {
@@ -9,6 +10,15 @@ namespace SVNAnalyser
     {
         static void Main(string[] args)
         {
+            string path = @"C:\\Apps\\Dev\\Code\\2017_Q1_0\\db\\stored_objects\\star_oracle\\ongoing_fees_process";
+            
+            Analyser analyser = new Analyser();
+
+			analyser.analyse(path);
+
+			Console.WriteLine("Complete. Press any key to exit.");
+
+			Console.ReadKey();		
         }
     }
 }
