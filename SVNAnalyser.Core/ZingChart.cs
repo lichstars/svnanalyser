f9f263e490541add65e40d5bba322ff7a929f56c
Liching Li <P696328@aur.national.com.au>
2017-02-07 14:56:41 +1100

    Add new ZingChart class which will match the JSON definition

---
+++ SVNAnalyser.Core/ZingChart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVNAnalyser.Core
+{
+    public class ZingChart
+    {
+        public List<string> graphset;
+
+        public ZingChart()
+        {
+            graphset = new List<string>();
+        }
+    }
+}
