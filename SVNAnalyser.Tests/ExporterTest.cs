c98c4f9f696083e5adadfbad39c93ab1d63252ac
Liching Li <P696328@aur.national.com.au>
2017-02-07 14:16:48 +1100

    Add ExportTest test meta data

---
+++ SVNAnalyser.Tests/ExporterTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SVNAnalyser.Tests
+{
+    [TestClass]
+    public class ExporterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
