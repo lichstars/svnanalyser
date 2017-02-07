b951e31d356d2f536c434839e2d82601ab9e8987
Liching Li <P696328@aur.national.com.au>
2017-02-07 12:12:36 +1100

    Add core library with Analyser class

---
+++ SVNAnalyser.Core/Analyser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVNAnalyser.Core
+{
+    public class Analyser
+    {
+        public void analyse(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
