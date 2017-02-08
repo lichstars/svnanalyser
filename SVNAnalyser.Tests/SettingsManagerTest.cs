289b9ce52959f7d846945dbf5d0d0fb798d7e27c
aaronmboyd <aaronmboyd@gmail.com>
2017-02-08 21:57:10 +1100

    Added unit test class for SettingsManager
    Stub for failing unit test assertions on SettingsManager

---
+++ SVNAnalyser.Tests/SettingsManagerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SVNAnalyser.Tests
+{
+    [TestClass]
+    public class SettingsManagerTest
+    {
+        [TestMethod]
+        public void SettingsManger_WillReturnPathToAnalyse_AsString()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SettingsManger_WillReturnPathToAnalyse_AsExistingPath()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SettingsManger_WillReturnPathToAnalyse_AsSVNPath()
+        {
+            Assert.Fail();
+        }
+    }
+}
