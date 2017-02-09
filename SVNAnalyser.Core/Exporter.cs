fa18aefec41ae6f24f1f718ccae89733e25b6730
Liching Li <P696328@aur.national.com.au>
2017-02-09 16:16:12 +1100

    Add entry point into creating zingcharts

--- SVNAnalyser.Core/Exporter.cs
+++ SVNAnalyser.Core/Exporter.cs
@@ -7,10 +7,31 @@ namespace SVNAnalyser.Core
 {
     public class Exporter
     {
-        /* Create a string serialised from the argument graphset and write to a file located at argument outputPath*/
-        public void forZingChart(ZingChart.Graphset graphset, string outputPath)
+        /* This function takes 2 arguments.
+         * 1. the output file name where the file will be saved to
+         * 2. an object/list of data which contains the class names and a list of developers that have touched that file with their % of code changes
+         * For each file/class that it finds in the list, 
+         * this method create a zingchart sending it the class name as the title and the list of developers with their respective %
+         * On completion, the zingchart gets converted to a graphset and serialised
+         * This serialised string is then written to the output file saved in outputPath
+         */
+        public void asZingChart(string outputPath, List<PlotData> plotData)
         {
-            throw new NotImplementedException();
+            ZingChart zingchart = new ZingChart();
+
+            foreach (PlotData data in plotData)
+            {
+                ZingChart.Chart chart = new ZingChart.Chart(data.title);
+
+                foreach (PlotData.Plot d in data.plots)
+                    chart.addSlice(d.percentage, d.developer);
+
+                zingchart.addChart(chart);
+            }
+
+            ZingChart.Graphset graphset = zingchart.getGraphSet();
+
         }
+
     }
 }
