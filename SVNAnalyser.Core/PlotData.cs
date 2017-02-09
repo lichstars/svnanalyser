feac33428edc62fca1ac568227436d84c774f149
Liching Li <P696328@aur.national.com.au>
2017-02-09 16:20:48 +1100

    Add new class PlotData to hold graph data

---
+++ SVNAnalyser.Core/PlotData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVNAnalyser.Core
+{
+    public class PlotData
+    {
+        public string title;
+        public List<Plot> plots = new List<Plot>();
+
+        public class Plot
+        {
+            public double percentage;
+            public string developer;
+
+            public Plot(double values, string text)
+            {
+                this.percentage = values;
+                this.developer = text;
+            }
+        }
+        
+    }
+}
