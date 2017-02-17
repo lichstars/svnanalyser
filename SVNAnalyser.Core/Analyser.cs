e5ddd6dcd62ae526270937483e6638447c814e08
aaronmboyd <aaronmboyd@gmail.com>
2017-02-17 16:41:56 +1100

    Initial unit tests for Analyser
    Added simple true/false unit tests for a valid ("C:\Apps\Dev\Code\star_tools\") and invalid ("C:\") SVN path. Hardcoded input paths at this point.

--- SVNAnalyser.Core/Analyser.cs
+++ SVNAnalyser.Core/Analyser.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Configuration;
 
 namespace SVNAnalyser.Core
 {
     public class Analyser
     {
-        public void analyse(string path)
+        public bool analyse(string path)
         {
-            throw new NotImplementedException();
+			//string logPrefix = System.Reflection.MethodBase.GetCurrentMethod().ReflectedType.Name + "." + System.Reflection.MethodBase.GetCurrentMethod().Name + " : ";
+
+			bool success = true;
+			string arguments = "blame " + path + " --xml";
+
+			Process pProcess = new Process();
+			pProcess.StartInfo.FileName = @"C:\Program Files\TortoiseSVN\bin\svn.exe";
+			pProcess.StartInfo.Arguments = arguments;
+			pProcess.StartInfo.UseShellExecute = false;
+			pProcess.StartInfo.RedirectStandardOutput = true;
+			pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden; //System.Diagnostics.ProcessWindowStyle.Normal
+			pProcess.StartInfo.CreateNoWindow = true;
+			pProcess.Start();
+			string result = pProcess.StandardOutput.ReadToEnd();
+			pProcess.WaitForExit();
+
+			if (!(result.Length > 0))
+				success = false;
+
+			if (!(result.Contains("<blame>") && result.Contains("</blame>")))
+				success = false;
+
+			return success;
         }
     }
 }
--- SVNAnalyser.Tests/AnalyserTest.cs
+++ SVNAnalyser.Tests/AnalyserTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SVNAnalyser.Core;
 
 namespace SVNAnalyser.Tests
 {
@@ -10,8 +11,21 @@ namespace SVNAnalyser.Tests
     public class AnalyserTest
     {
         [TestMethod]
-        public void Analyser_Test_1()
+        public void Analyser_WillReturnTrue_WhenAnalyseWorkingCopy()
         {
+			string pathToAnalyse = @"C:\Apps\Dev\Code\star_tools\";
+			Analyser analyser = new Analyser();
+			bool success = analyser.analyse(pathToAnalyse);
+			Assert.AreEqual(success, true);
         }
+
+		[TestMethod]
+		public void Analyser_WillReturnFalse_WhenAnalyseNotAWorkingCopy()
+		{
+			string pathToAnalyse = @"C:\";
+			Analyser analyser = new Analyser();
+			bool success = analyser.analyse(pathToAnalyse);
+			Assert.AreEqual(success, false);
+		}
     }
 }
